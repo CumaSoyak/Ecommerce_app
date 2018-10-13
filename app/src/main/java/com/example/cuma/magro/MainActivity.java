1f09cc3872adf5e00f315a99bd26f0a446d910bb
CumaSoyak <cumasoyak51@gmail.com>
2018-10-13 22:11:04 +0300

    Fragmentler eklendi

--- app/src/main/java/com/example/cuma/magro/MainActivity.java
+++ app/src/main/java/com/example/cuma/magro/MainActivity.java
@@ -2,43 +2,74 @@ package com.example.cuma.magro;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
+
+import com.example.cuma.magro.Fragment.HomeFragment;
+import com.example.cuma.magro.Fragment.SearchFragment;
+import com.example.cuma.magro.Fragment.ShopFragment;
+import com.example.cuma.magro.Fragment.UserFragment;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private ImageView sepet, menu;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sepet = findViewById(R.id.menu);
+        menu = findViewById(R.id.sepet);
+        sepet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Tıkladınız",Toast.LENGTH_LONG).show();
+            }
+        });
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.ifRoom,new HomeFragment()).commit();
     }
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment=null;
+            switch (item.getItemId()) {
+                case R.id.navigation_anasayfa:
+                    selectedFragment=new HomeFragment();
+                    return true;
+                case R.id.navigation_urunler:
+                    selectedFragment=new ShopFragment();
+                    return true;
+                case R.id.navigation_arama:
+                    selectedFragment=new SearchFragment();
+                    return true;
+                case R.id.navigation_hesap:
+                    selectedFragment=new UserFragment();
+                    return true;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.ifRoom,selectedFragment).commit();
+
+            return false;
+        }
+    };
+
 
 }
